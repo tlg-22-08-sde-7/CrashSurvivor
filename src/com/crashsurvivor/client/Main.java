5562defd28c7255d0d6d7c45062f2ad33fd878c7
Nima Gopali <nimagopali@gmail.com>
2022-12-01 17:10:39 -0500

    Welcome screen method, ascii and console.clear(), AB#273

--- src/com/crashsurvivor/client/Main.java
+++ src/com/crashsurvivor/client/Main.java
@@ -1,9 +1,11 @@
 package com.crashsurvivor.client;
 
+import com.apps.util.Console;
 import com.crashsurvivor.app.GameBoard;
 
 class Main {
     public static void main(String[] args) {
+        Console.clear();
         GameBoard board = new GameBoard();
         board.execute();
     }
