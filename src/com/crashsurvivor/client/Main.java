2cd946083eb6641e22067f9f8128581a1f1dd232
Nima Gopali <nimagopali@gmail.com>
2022-12-01 14:12:39 -0500

    Added packages, Main class, GameBoard class, AB#270

---
+++ src/com/crashsurvivor/client/Main.java
@@ -0,0 +1,10 @@
+package com.crashsurvivor.client;
+
+import com.crashsurvivor.app.GameBoard;
+
+class Main {
+    public static void main(String[] args) {
+        GameBoard board = new GameBoard();
+        board.execute();
+    }
+}
