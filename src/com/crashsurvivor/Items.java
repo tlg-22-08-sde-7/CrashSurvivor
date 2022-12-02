95deec777595667f9df3681851c1cd17eab151c1
Nima Gopali <nimagopali@gmail.com>
2022-12-02 13:56:36 -0500

    Added Inventory, Items, KeyItems classes with fields and methods, AB#262

---
+++ src/com/crashsurvivor/Items.java
@@ -0,0 +1,15 @@
+package com.crashsurvivor;
+
+class Items {
+    private String itemName;
+
+    private void addItem(){
+
+    }
+
+    private void removeItem(){
+
+    }
+
+
+}
---
+++ src/com/crashsurvivor/KeyItems.java
@@ -0,0 +1,14 @@
+package com.crashsurvivor;
+
+class KeyItems {
+    private String keyItemName;
+
+    private void addKeyItem(){
+
+    }
+
+    private void removeKeyItem(){
+
+    }
+
+}
