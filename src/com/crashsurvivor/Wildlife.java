6d6eb7baca7e31be402ce7d138d12e60fafc9d84
Nima Gopali <nimagopali@gmail.com>
2022-12-02 13:37:26 -0500

    Added Player and Wildlife structure, AB#262

---
+++ src/com/crashsurvivor/Wildlife.java
@@ -0,0 +1,45 @@
+package com.crashsurvivor;
+
+class Wildlife {
+    private String name;
+    private int hp;
+    private int attackPoints;
+
+    public Wildlife(String name, int hp, int attackPoints){
+        setName(name);
+        setHp(hp);
+        setAttackPoints(attackPoints);
+    }
+
+    private void attack(){
+
+    }
+
+    private void retreat(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public void setAttackPoints(int attackPoints) {
+        this.attackPoints = attackPoints;
+    }
+}
